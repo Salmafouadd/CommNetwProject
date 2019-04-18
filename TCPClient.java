55a917b863479108dcafd8df85719f69df4084e6
Salmafouadd <49488106+Salmafouadd@users.noreply.github.com>
2019-04-18 19:06:09 +0200

    Add files via upload

--- TCPClient.java
+++ TCPClient.java
@@ -1,42 +1,51 @@
 package networks;
 
-
 import java.io.*; 
 import java.net.*; 
 class TCPClient { 
 
     public static void main(String argv[]) throws Exception 
     { 
-        String sentence; 
-        String modifiedSentence; 
+       
+       String sentence=""; 
+       String modifiedSentence=""; 
+       
+       BufferedReader inFromUser = 
+    	          new BufferedReader(new InputStreamReader(System.in)); 
 
-        BufferedReader inFromUser = 
-          new BufferedReader(new InputStreamReader(System.in)); 
+    	   Socket clientSocket = new Socket("192.168.43.71", 6789); 
 
-        Socket clientSocket = new Socket("127.0.0.1", 6789); 
+    	   DataOutputStream outToServer = 
+    	          new DataOutputStream(clientSocket.getOutputStream()); 
+    	   
+    	   BufferedReader inFromServer = 
+    	                new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
 
-        DataOutputStream outToServer = 
-          new DataOutputStream(clientSocket.getOutputStream()); 
-        BufferedReader inFromServer = 
-                new BufferedReader(new
-                InputStreamReader(clientSocket.getInputStream())); 
-
-              sentence = inFromUser.readLine(); 
-
-              outToServer.writeBytes(sentence + '\n'); 
-
-              modifiedSentence = inFromServer.readLine(); 
-
-              System.out.println("FROM SERVER: " + modifiedSentence); 
-
-              clientSocket.close(); 
-                         
-          } 
-      } 
-
-
-
-
-
-
+    	   sentence = inFromUser.readLine();
+    	   
+    	   if(!sentence.equals("connect")){
+    		   System.out.println("Please connect first and try again");
+    		   clientSocket.close();
+    	   }
+	while(sentence.equals("connect")) {
+		
+		while(!sentence.equals("end")){
+			
+		     sentence = inFromUser.readLine();
+		     if(sentence.equals("end")) {
+		    	 	clientSocket.close(); 
+		    	 	break;
+		     }
+		     else {
+			     outToServer.writeBytes(sentence + "\n");
+			     modifiedSentence = inFromServer.readLine();
+			     System.out.println("FROM SERVER: " + modifiedSentence);
+		     }
+	     	}
+		 clientSocket.close(); 
+		}
+	       
+	       
+    }
+    }
 
