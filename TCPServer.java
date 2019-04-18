611849f7c2b1514076502e634dcb80deffac21c2
Salmafouadd <49488106+Salmafouadd@users.noreply.github.com>
2019-04-18 19:05:21 +0200

    Add files via upload

--- TCPServer.java
+++ TCPServer.java
@@ -1,35 +1,45 @@
-package networks;
+ package networks;
 
 import java.io.*; 
 import java.net.*; 
 
 class TCPServer { 
+	
 
-  public static void main(String argv[]) throws Exception 
+public static void main(String argv[]) throws Exception 
     { 
-      String clientSentence; 
-      String capitalizedSentence; 
+      String clientSentence=""; 
+      String capitalizedSentence=""; 
 
-      ServerSocket welcomeSocket = new ServerSocket(6789); 
-  
+ //  try {
+    	ServerSocket welcomeSocket = new ServerSocket(6789);   
+      Socket connectionSocket = welcomeSocket.accept(); 
+      
       while(true) { 
-  
-            Socket connectionSocket = welcomeSocket.accept(); 
+	  		
+         
+          BufferedReader inFromClient = 
+             new BufferedReader(new
+             InputStreamReader(connectionSocket.getInputStream())); 
 
-           BufferedReader inFromClient = 
-              new BufferedReader(new
-              InputStreamReader(connectionSocket.getInputStream())); 
-           DataOutputStream  outToClient = 
-                   new DataOutputStream(connectionSocket.getOutputStream()); 
-
-                 clientSentence = inFromClient.readLine(); 
-
-                 capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
-
-                 outToClient.writeBytes(capitalizedSentence); 
-              } 
-          } 
-      } 
+          DataOutputStream  outToClient = 
+                  new DataOutputStream(connectionSocket.getOutputStream()); 
+          
+          clientSentence = inFromClient.readLine();   
+          if(clientSentence!=null){
+        	  if (!clientSentence.equals("end")) {        		                 
+               	 capitalizedSentence = clientSentence.toUpperCase() + '\n';
+               	 outToClient.writeBytes(capitalizedSentence); 
+        		  }
+        	  else {
+        		  connectionSocket.close();
+             } 
+     	}
        
 
+    
+      } 
+    } 
+}
+       
 
